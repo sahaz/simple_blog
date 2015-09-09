0da1c68421a3610e2e00d99428aecd0d6cc5c3d6
sahaz <sahazbhattarai@gmail.com>
2015-09-09 07:17:11 +0545

    Upto comment completed

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,60 @@
+class PostsController < ApplicationController
+  def index
+    @posts=Post.all
+  end
+  def new
+    @post=Post.new
+  end
+
+   def show
+    @post=Post.find(params[:id])
+  end
+  def edit
+    @post=Post.find(params[:id])
+  end
+  
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new } # :new bhanne method ma jancha 
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @purchase, notice: 'Purchase was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+ 
+  redirect_to posts_path
+end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
+end
